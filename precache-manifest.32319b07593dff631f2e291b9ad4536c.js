7bbe7b0fbbd4d89f1b8e5acc28d20dfd3d5dd9a5
Clayton Puno <claytonpuno@gmail.com>
2020-08-14 01:28:15 -0400

    Updates

---
+++ precache-manifest.32319b07593dff631f2e291b9ad4536c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d520ebb3035395944aff72a5e07f5ae",
+    "url": "/rpg-generator/index.html"
+  },
+  {
+    "revision": "09367defab04c8af6e47",
+    "url": "/rpg-generator/static/css/main.0b32522d.chunk.css"
+  },
+  {
+    "revision": "d2489050cc6790fc8b4a",
+    "url": "/rpg-generator/static/js/2.f0350789.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "/rpg-generator/static/js/2.f0350789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09367defab04c8af6e47",
+    "url": "/rpg-generator/static/js/main.b27a6a1f.chunk.js"
+  },
+  {
+    "revision": "babd7027a225206a3e26",
+    "url": "/rpg-generator/static/js/runtime-main.d1e14dce.js"
+  }
+]);
